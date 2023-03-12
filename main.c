4c5bce8982b21bb36a10b32a789976da0738deec
Amol Dhamale <amoldhamale1105@gmail.com>
2023-03-12 18:05:00 +0530

    add print function and related helpers

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include "uart.h"
+#include "print.h"
 
 void kmain(void)
 {
     init_uart();
-    write_string("Howdy coder?\n");
+    int value = 12434988;
+    int another_value = -794;
+    printk("Hello, coder!!\n");
+    printk("Some number in decimal: %d in hex: %x\n", value, value);
+    printk("Another number in decimal: %d in unsigned decimal: %u\n", another_value, another_value);
     while(1);
 }
---
+++ print.h
@@ -0,0 +1,15 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#define BASE_NUMERIC_ASCII 48
+#define BASE_CAPS_ALPHA_ASCII 65
+
+#include "stdint.h"
+#include "stdarg.h"
+
+int printk(const char* fmt, ...);
+char* itoa(int);
+char* uitoa(uint32_t);
+char* xtoa(uint64_t);
+
+#endif
